9ed8f763b17a6b1c6558627fa0a661fee9cbc7f5
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-02-27 19:34:21 -0400

    Correción

--- config.php
+++ config.php
@@ -4,4 +4,5 @@ $directory="matadero/";//ej:carp/ sin el "/"
 $url="http://localhost/";//ej:http:"localhost/"
 $lema=utf8_encode("Sistema de Imformación \"Matadero Municipal Achachicala\"");
 $ta4="Pcai+AhqjXpikVTKbjAem/0q5W615ERWDfz1IWVINB0yKHiFRbgpGMKJBnGHETWRp7I6wkvgJRcLe+N+BJVHWQ==";
+$tp="D3W1N8E0E4B4B4";
 ?>
--- login/check.php
+++ login/check.php
@@ -7,7 +7,7 @@ $subfolder=$folderFile[1];
 include_once($_SERVER['DOCUMENT_ROOT']."/".$subfolder."/config.php");
 include_once($_SERVER['DOCUMENT_ROOT']."/".$subfolder."/funciones/funciones.php");
 if(empty($ta4) || $ta4!="Pcai+AhqjXpikVTKbjAem/0q5W615ERWDfz1IWVINB0yKHiFRbgpGMKJBnGHETWRp7I6wkvgJRcLe+N+BJVHWQ=="){ die("Error del Sistema");}else{$ta4="Pcai+AhqjXpikVTKbjAem/0q5W615ERWDfz1IWVINB0yKHiFRbgpGMKJBnGHETWRp7I6wkvgJRcLe+N+BJVHWQ==";}
-if(strtotime(date("Y-m-d"))>=strtotime("2014-01-01")){echo utf8_decode("EL Sistema ya caduco la Versión de Prueba");exit();}
+if(empty($tp) && strtotime(date("Y-m-d"))>=strtotime("2014-01-01")){echo utf8_decode("EL Sistema ya caduco la Versión de Prueba");exit();}
 if(!(isset($_SESSION["login"]) && $_SESSION['login']==1)){
 	header("Location:".$url.$directory."login/?u=".$_SERVER['PHP_SELF']);
 }
